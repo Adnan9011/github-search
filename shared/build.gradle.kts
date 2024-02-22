52e7bf8e910f93d49f11fe192aaa9f84ea40622b
Adnan abdollah zaki <adnan9011@gmail.com>
2024-02-22 11:09:53 +0330

    feat: Update shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,42 +1,85 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.compose)
 }
 
 kotlin {
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "1.8"
-            }
-        }
-    }
-    
+    androidTarget()
     listOf(
         iosX64(),
         iosArm64(),
         iosSimulatorArm64()
     ).forEach {
         it.binaries.framework {
-            baseName = "shared"
+            baseName = "feature"
             isStatic = true
         }
     }
 
     sourceSets {
         commonMain.dependencies {
-            //put your multiplatform dependencies here
+            projects.shared.apply {
+                implementation(domain)
+                implementation(core)
+            }
+            compose.apply {
+                implementation(runtime)
+                implementation(foundation)
+                implementation(animation)
+                implementation(material3)
+            }
+            libs.coil.apply {
+                implementation(core)
+                implementation(compose)
+            }
+            libs.ktor.apply {
+                implementation(core)
+            }
+            implementation(libs.coroutine)
+            libs.koin.apply {
+                implementation(core)
+                implementation(compose)
+            }
+            implementation(libs.koin.compose)
+            libs.mvvm.apply {
+                implementation(compose)
+                implementation(compose.flow)
+            }
+            implementation(libs.kotlinx.collections.immutable)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+        }
+        androidMain.dependencies {
+            compose.apply {
+                implementation(preview)
+                implementation(uiTooling)
+            }
+            implementation(libs.koin.android)
         }
     }
 }
 
 android {
     namespace = "com.adnan.app"
-    compileSdk = 34
+    compileSdk = libs.versions.app.compile.sdk.get().toInt()
     defaultConfig {
-        minSdk = 24
+        minSdk = libs.versions.app.min.sdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        jvmToolchain(libs.versions.java.jdk.get().toInt())
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.android.compiler.get()
     }
 }
