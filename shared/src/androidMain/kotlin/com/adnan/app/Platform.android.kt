c842f9b1c7f9da4d131e25b3e89b61687c56c169
Adnan abdollah zaki <adnan9011@gmail.com>
2024-02-22 15:21:22 +0330

    feat: Remove default classes

--- shared/src/androidMain/kotlin/com/adnan/app/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package com.adnan.app
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- shared/src/commonMain/kotlin/com/adnan/app/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.adnan.app
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
