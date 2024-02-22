e334471bdc3a01577ee9cc7d9a6b0538387795d4
Adnan abdollah zaki <adnan9011@gmail.com>
2024-02-22 17:02:00 +0330

    feat: Fix package for kmp module

--- shared/core/src/androidTest/java/com/adnan/core/ExampleInstrumentedTest.kt
+++
@@ -1,9 +0,0 @@
-package com.adnan.core
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleInstrumentedTest {
-}
